cab72153d67c482112e33a727b33a148c2931403
Bryan Logicsa <bryanguevara38@gmail.com>
2021-06-17 11:20:42 -0600

    trabajando los scripts de pdf hemco para adjuntar al proyecto

---
+++ pdf_hemco/pdf_hemco.py
@@ -0,0 +1,54 @@
+import os, time
+import pandas as pd
+# openpyxl
+
+"""path = os.chdir('C:/Users/Santiago/Desktop/change_name')
+archivo = 'C:/Users/Santiago/Desktop/DATA.xlsx' # Nombre del archivo de nombres
+contador = 0 # Unicamente con la funcion de contar la cantidad de archivos
+name_file = [] # Save all file name
+nombre_anterior = []
+new = []
+
+def generador():
+	d = pd.read_excel(archivo, sheet_name='Hoja1')
+	df = pd.DataFrame(data=d)
+	documents = []
+
+	for i in df['NOMBRE DOCUMENTO']:
+		yield i
+
+r = generador()
+
+# Separa los archivos pdf con el programa '.py'
+for f in os.listdir(path):
+	name, typeFile = os.path.splitext(f)
+	if typeFile == '.py':
+		continue
+	else:
+		name_file.append(name + typeFile)
+		nombre_anterior.append(name + typeFile)
+		contador += 1 # Cuenta la cantidad de archivos ejecutados
+
+#name_file.sort(key=os.path.getctime)
+
+# here chane name this file's
+for file in name_file:
+	name = file.replace(file,next(r))
+	new_name = name + '.pdf'
+	new.append(new_name)
+	os.replace(file,new_name)"""
+
+
+"""for i in new:
+	# print( "Nombre: ", i ,"\nFecha: "," {}".format(time.ctime(os.path.getctime(i))))
+	# print('Nombre anterior: ', i, '\nNumero nombre: ',next(data))
+	print(i)"""
+
+p = os.getcwd()
+li = os.listdir(p)
+
+for i in li:
+	print(i, "\n")
+
+print("p1", len(li) )
+#print("p2", p2 )
